7f03f803c90d38fe4bebbc4e89e02eecd185edf4
hardliner66 <hardliner66@gmail.com>
2020-09-01 10:19:52 +0200

    change build directory to temp & create temp dir

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-fennel --require-as-include --compile src/main.fnl > build/build.lua
-amalg.lua -s entry.lua build/build -o build/package.lua
-luasrcdiet --quiet --maximum --noopt-comments build/package.lua -o build/package_min.lua
+mkdir temp
+
+fennel --require-as-include --compile src/main.fnl > temp/build.lua
+amalg.lua -s entry.lua temp/build -o temp/package.lua
+luasrcdiet --quiet --maximum --noopt-comments temp/package.lua -o temp/package_min.lua
 
 SLOTS=$(cat SLOTS)
 SLOTS=$(echo $SLOTS | tr '\r\n' ' ')
 SLOTS=$(echo $SLOTS | tr '\n' ' ')
 
-tools/wrap.lua build/package_min.lua --slots $SLOTS --handle-errors > out/final.json
+tools/wrap.lua temp/package_min.lua --slots $SLOTS --handle-errors > out/final.json
